3228b1c70e365aeae376a0445784e7f9ab2626e6
ayiskakov <ayiskakov@gmail.com>
2026-09-21 09:11:02 +0000

    Release 11.8.4: bump version (patch) + build numbers + What's New

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.noop.whoop"
         minSdk = 26
         targetSdk = 34
-        versionCode = 532
-        versionName = "11.8.3"
+        versionCode = 533
+        versionName = "11.8.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
